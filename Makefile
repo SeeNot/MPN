dca2b23a7db1fb980a5686735868237be809282a
SeeNot <142214624+SeeNot@users.noreply.github.com>
2026-06-24 20:32:02 +0300

    Update dependecies

--- Makefile
+++ Makefile
@@ -4,4 +4,4 @@ lint:
 
 # Format, lint, and organize imports of all files
 format:
-    deno run -A npm:@biomejs/biome check --write
+	deno run -A npm:@biomejs/biome check --write
--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -2,31 +2,31 @@ import { useAuth } from "../../features/auth/AuthContext.tsx";
 import { useCurrentUser } from "../../hooks/useSpotify";
 
 function Dashboard() {
-	const { logout } = useAuth();
-	const { data: user, isLoading, error } = useCurrentUser();
-	// 2. Handle states easily
-	if (isLoading)
-		return (
-			<>
-				<div className="text-white">Loading profile...</div>
-				<button type="submit" onClick={logout}>
-					Logout
-				</button>
-			</>
-		);
-	if (error) return <div className="text-red-500">Error loading data</div>;
+  const { logout } = useAuth();
+  const { data: user, isLoading, error } = useCurrentUser();
 
-	return (
-		<>
-			<h1>works</h1>
-			<button type="submit" onClick={logout}>
-				Logout
-			</button>
-			<div className="mt-4">
-				<p>Followers: {user?.followers.total}</p>
-			</div>
-		</>
-	);
+  if (isLoading)
+    return (
+      <>
+        <div className="text-white">Loading profile...</div>
+        <button type="submit" onClick={logout}>
+          Logout
+        </button>
+      </>
+    );
+  if (error) return <div className="text-red-500">Error loading data</div>;
+
+  return (
+    <>
+      <h1>works</h1>
+      <button type="submit" onClick={logout}>
+        Logout
+      </button>
+      <div className="mt-4">
+        <p>Followers: {user?.followers.total}</p>
+      </div>
+    </>
+  );
 }
 
 export default Dashboard;
